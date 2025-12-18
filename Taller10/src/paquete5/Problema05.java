873c970c582df144c2d0bbeafdd9bb03ddf6b137
CodebyChris07 <chrisjalu07@gmail.com>
2025-12-18 12:51:09 -0500

    resolucion parcial del taller

--- Taller10/src/paquete5/Problema05.java
+++ Taller10/src/paquete5/Problema05.java
@@ -19,41 +19,34 @@ import java.util.Locale;
  * @author reroes
  */
 public class Problema05 {
+
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
         entrada.useLocale(Locale.US);
-    String[][] estudiantes = {{"Sarah Ward", "Philip Payne"},
- {"Carrie Burton", "Lauren Rice"},
-{"Emma Escobar", "Lori Flores"},
-{"Steven West", "Toni Martin"}
- };
-    String reporte = "";
-    String inicial;
-    for(int fila = 0; fila < estudiantes.length; fila++){
-    for (int col = 0; col < estudiantes[fila].length; col++){
-    String nombre = estudiantes[fila][col];
-    inicial = nombre.substring(0,1);
-    switch(inicial){
-        case "S":
-        case "P":
-        case "T":
-        reporte = String.format("%s%s\n", reporte, nombre);
-        break;
-    
+        String[][] estudiantes = {{"Sarah Ward", "Philip Payne"},
+        {"Carrie Burton", "Lauren Rice"},
+        {"Emma Escobar", "Lori Flores"},
+        {"Steven West", "Toni Martin"}
+        };
+        String reporte = "";
+        String inicial;
+        for (int fila = 0; fila < estudiantes.length; fila++) {
+            for (int col = 0; col < estudiantes[fila].length; col++) {
+                String nombre = estudiantes[fila][col];
+                inicial = nombre.substring(0, 1);
+                switch (inicial) {
+                    case "S":
+                    case "P":
+                    case "T":
+                        reporte = String.format("%s%s\n", reporte, nombre);
+                        break;
+
+                }
+                System.out.printf(reporte);
+
+            }
+
+        }
+
     }
-    System.out.printf(reporte);
-    
-    }
-    
-    
-    
-    
-    
-    
-    }
-    
-    
-    
-    
-    }
-    }
+}
--- Taller10/src/paquete6/Problema06.java
+++ Taller10/src/paquete6/Problema06.java
@@ -1,9 +1,21 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ Diseñe un algoritmo que permita leer un número entero o real y, posteriormente,
+ingresar los valores numéricos de una matriz de 5 filas y 6 columnas.
+Una vez ingresados los datos, el algoritmo debe:
+Multiplicar cada elemento de la matriz original por el número leído.
+Almacenar el resultado de cada multiplicación en la misma posición de una nueva 
+matriz, llamada matriz resultante.
+Mostrar en pantalla la matriz original.
+Mostrar en pantalla la matriz resultante, producto de la multiplicación por el 
+número ingresado.
+Cada elemento de la matriz resultante debe corresponder al producto entre el 
+número ingresado y el valor ubicado en la misma fila y columna de la matriz 
+original.
  */
 package paquete6;
+
+import java.util.Scanner;
+import java.util.Locale;
 
 /**
  *
@@ -11,4 +23,53 @@ package paquete6;
  */
 public class Problema06 {
 
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        entrada.useLocale(Locale.US);
+        double num;
+        double[][] matriz1 = new double[5][6]; // matriz original
+        double[][] matrizResul = new double[5][6]; // matriz resultante
+
+        String reporte = "";
+
+        System.out.println("Ingrese un número entero o real: ");
+        num = entrada.nextDouble();
+
+        System.out.println("\nIngrese los valores de la matriz (5x6):");
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 6; j++) {
+                System.out.printf("Valor [%d][%d]: ", i, j);
+                matriz1[i][j] = entrada.nextDouble();
+            }
+        }
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 6; j++) {
+                matrizResul[i][j] = matriz1[i][j] * num;
+            }
+        }
+
+        reporte = String.format("%s\nMatriz Original:\n", reporte);
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 6; j++) {
+                reporte = String.format("%s%.2f ", reporte,
+                        matriz1[i][j]);
+            }
+            reporte = String.format("%s\n", reporte);
+        }
+
+        reporte = String.format("%s\nMatriz Resultante (multiplicada "
+                + "por %.2f):\n",
+                reporte, num);
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 6; j++) {
+                reporte = String.format("%s%.2f ", reporte,
+                        matrizResul[i][j]);
+            }
+            reporte = String.format("%s\n", reporte);
+        }
+
+        System.out.println(reporte);
+    }
 }
